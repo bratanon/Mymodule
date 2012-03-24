5746cfbf807504df9807fdd2f5cbd567a7dc55f0
Emil Stjerneman <emil@stjerneman.com>
2012-03-24 03:01:35 +0100

    Added a demo entity.

---
+++ plugins/linkit_plugins/myplugin-entity.class.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ * Define Linkit node plugin class.
+ */
+class LinkitPluginMymodule_entity extends LinkitPluginEntity {
+
+  /**
+   * Return a string representing this handler's name in the UI.
+   */
+  function ui_title() {
+    return '--' . check_plain($this->plugin['ui_title']) . ' --';
+  }
+
+  /**
+   * Generate a settings form for this handler.
+   * Uses the standard Drupal FAPI.
+   * The element will be attached to the "data" key.
+   *
+   * @return
+   *   An array containing any custom form elements to be displayed in the
+   *   profile editing form.
+   */
+  function buildSettingsForm() {
+    // Get the parent settings form.
+    $form = parent::buildSettingsForm();
+
+    $form[$this->plugin['name']]['foo'] = array(
+      '#title' => t('A settings for mymodule_entity'),
+      '#type' => 'checkbox',
+      '#default_value' => isset($this->conf['foo']) ? $this->conf['foo'] : 0,
+    );
+
+    return $form;
+  }
+}
